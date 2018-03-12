df175d7e8cd4730c03e3f71795b3f1c07377d420
Jose Ayerdis <necronet@Joses-MacBook-Pro.local>
2018-03-12 10:01:49 -0400

    Add ridge regression algorithm

--- basecode/linear_regression.py
+++ basecode/linear_regression.py
@@ -16,3 +16,47 @@ def testOLERegression(w,Xtest,ytest):
     N = ytest.shape[0]
     mse = np.sum( np.square( ytest.T - (np.dot(w.T,Xtest.T)) ) ) / N
     return mse
+
+def learnRidgeRegression(X,y,lambd):
+    rigde_term = lambd * np.identity(X.shape[1])
+    w = np.dot( np.dot( np.linalg.inv(np.dot( X.T, X ) + rigde_term ), X.T), y )
+    return w
+
+if sys.version_info.major == 2:
+    X,y,Xtest,ytest = pickle.load(open('diabetes.pickle','rb'))
+else:
+    X,y,Xtest,ytest = pickle.load(open('diabetes.pickle','rb'),encoding = 'latin1')
+
+# add intercept
+X_i = np.concatenate((np.ones((X.shape[0],1)), X), axis=1)
+Xtest_i = np.concatenate((np.ones((Xtest.shape[0],1)), Xtest), axis=1)
+
+w = learnOLERegression(X,y)
+mle = testOLERegression(w,Xtest,ytest)
+
+w_i = learnOLERegression(X_i,y)
+mle_i = testOLERegression(w_i,Xtest_i,ytest)
+
+print('MSE without intercept '+str(mle))
+print('MSE with intercept '+str(mle_i))
+
+# Problem 3
+k = 101
+lambdas = np.linspace(0, 1, num=k)
+i = 0
+mses3_train = np.zeros((k,1))
+mses3 = np.zeros((k,1))
+for lambd in lambdas:
+    w_l = learnRidgeRegression(X_i,y,lambd)
+    mses3_train[i] = testOLERegression(w_l,X_i,y)
+    mses3[i] = testOLERegression(w_l,Xtest_i,ytest)
+    i = i + 1
+fig = plt.figure(figsize=[12,6])
+plt.subplot(1, 2, 1)
+plt.plot(lambdas,mses3_train)
+plt.title('MSE for Train Data')
+plt.subplot(1, 2, 2)
+plt.plot(lambdas,mses3)
+plt.title('MSE for Test Data')
+
+plt.show()
